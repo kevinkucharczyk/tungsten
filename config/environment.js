cb82d57c719a63dee4f515433c84e71a0d20cdcf
Kevin P. Kucharczyk <kevinkucharczyk@gmail.com>
2016-03-14 18:25:41 +0100

    Setup pod structure

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'tungsten',
+    podModulePrefix: 'tungsten/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
