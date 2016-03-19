803182f425b4bbac4af661daea3c2d09de95bdf1
Kevin P. Kucharczyk <kevinkucharczyk@gmail.com>
2016-03-19 13:31:12 +0100

    Add model tests

--- tests/unit/pods/category/model-test.js
+++ tests/unit/pods/category/model-test.js
@@ -1,12 +1,33 @@
+import Ember from 'ember';
 import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('category', 'Unit | Model | category', {
-  // Specify the other units that are required for this test.
   needs: []
 });
 
-test('it exists', function(assert) {
-  let model = this.subject();
-  // let store = this.store();
-  assert.ok(!!model);
+test('correctly identifies top level category', function(assert) {
+  const topLevelCategory = this.subject({
+    title: 'Top Level'
+  });
+
+  assert.ok(topLevelCategory.get('topLevel'));
+});
+
+test('correctly identifies non top level category', function(assert) {
+  let topLevelCategory;
+  let secondLevelCategory;
+
+  Ember.run(() => {
+    topLevelCategory = this.store().createRecord('category', {
+      id: 1,
+      title: 'Top Level'
+    });
+
+    secondLevelCategory = this.store().createRecord('category', {
+      title: '2nd Level',
+      parent: topLevelCategory
+    });
+  });
+
+  assert.notOk(secondLevelCategory.get('topLevel'));
 });
