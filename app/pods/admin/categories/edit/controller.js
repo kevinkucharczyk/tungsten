5781b242e1bb875f4073926bc5e491cb4c1be680
Kevin P. Kucharczyk <kevinkucharczyk@gmail.com>
2016-03-29 20:15:21 +0200

    Filter categories in edit to disallow setting parent to itself

--- app/pods/admin/categories/edit/controller.js
+++ app/pods/admin/categories/edit/controller.js
@@ -2,5 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   sortProps: ['categoryWithParents'],
-  sortedCategories: Ember.computed.sort('model.categories', 'sortProps')
+  sortedCategories: Ember.computed.sort('filteredCategories', 'sortProps'),
+
+  filteredCategories: Ember.computed.filter('model.categories', function(category) {
+    return category.get('id') !== this.get('model.category.id');
+  })
 });
